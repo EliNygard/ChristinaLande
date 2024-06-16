b278615540a2c068c17d394ca41a668ced1dbc82
EliNygard <eli.nygard@gmail.com>
2024-06-16 22:23:51 +0200

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".index.css",
+    "./index.html",
+    "./**/*.css",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

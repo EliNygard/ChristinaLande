ef0b6e213d7146256c76aaede3b291de0fdbec59
EliNygard <eli.nygard@gmail.com>
2024-07-16 18:20:01 +0200

    fixing nav menu

--- javascript/ui/topNav.mjs
+++ javascript/ui/topNav.mjs
@@ -1,64 +1,126 @@
 // source: https://www.youtube.com/watch?v=pBv7igaxfQE&t=8s
 
-function setUpTopNav(e) {
-    const topNavMenu = document.querySelector(".topnav__menu");
-    if (e.matches) {
-      //is mobile
-      topNavMenu.setAttribute("inert", "");
-      topNavMenu.style.transition = "none";
-    } else {
-      //is tablet/desktop
-      topNavMenu.removeAttribute("inert");
-      closeMobileMenu();
-    }
-  }
-  
+  const btnOpen = document.querySelector("#btnOpen");
+  const btnClose = document.querySelector("#btnClose");
+  const media = window.matchMedia("(width < 766px)");
+  const topNavMenu = document.querySelector(".topnav__menu");
+  const main = document.querySelector("main");
+  const body = document.querySelector("body");
+
   function openMobileMenu() {
-    const btnOpen = document.querySelector("#btnOpen");
-    const btnClose = document.querySelector("#btnClose");
-    const topNavMenu = document.querySelector(".topnav__menu");
-    const main = document.querySelector("main");
-    const body = document.querySelector("body");
     btnOpen.setAttribute("aria-expanded", "true");
-    topNavMenu.classList.toggle("open", true);
+    topNavMenu.classList.add("open");
     topNavMenu.removeAttribute("inert");
     topNavMenu.removeAttribute("style");
     main.setAttribute("inert", "");
     bodyScrollLockUpgrade.disableBodyScroll(body);
     btnClose.focus();
   }
-  
+
   function closeMobileMenu() {
-    const btnClose = document.querySelector("#btnClose");
-    const btnOpen = document.querySelector("#btnOpen");
-    const topNavMenu = document.querySelector(".topnav__menu");
-    const main = document.querySelector("main");
-    const body = document.querySelector("body");
-    btnClose.setAttribute("aria-expanded", "false");
-    topNavMenu.classList.toggle("open", false);
+    btnOpen.setAttribute("aria-expanded", "false");
+    topNavMenu.classList.remove("open");
     topNavMenu.setAttribute("inert", "");
     main.removeAttribute("inert");
     bodyScrollLockUpgrade.enableBodyScroll(body);
     btnOpen.focus();
-  
+
     setTimeout(() => {
       topNavMenu.style.transition = "none";
     }, 500);
   }
-  
-  function initializeTopNav() {
-    const btnClose = document.querySelector("#btnClose");
-    const btnOpen = document.querySelector("#btnOpen");
-    const media = window.matchMedia("(width <= 766px)");
-    setUpTopNav(media);
-  
-    btnOpen.addEventListener("click", openMobileMenu);
-    btnClose.addEventListener("click", closeMobileMenu);
-  
-    media.addEventListener("change", function (e) {
-      setUpTopNav(e);
-    });
+
+  function setupTopNav(e) {
+    if (e.matches) {
+      // is mobile
+      console.log("is mobile");
+      topNavMenu.setAttribute("inert", "");
+      topNavMenu.style.transition = "none";
+    } else {
+      // is tablet/desktop
+      console.log("is desktop");
+      closeMobileMenu();
+      topNavMenu.removeAttribute("inert");
+    }
   }
 
-  initializeTopNav()
+  setupTopNav(media);
+
+  btnOpen.addEventListener("click", () => {
+    console.log("open mobile menu");
+    openMobileMenu()
+  } );
+  btnClose.addEventListener("click", closeMobileMenu);
+
+  media.addEventListener("change", function (e) {
+    setupTopNav(e);
+  });
+
+
+
+//   const topNavMenu = document.querySelector(".topnav__menu");
+//   const btnOpen = document.querySelector("#btnOpen");
+//   const btnClose = document.querySelector("#btnClose");
+//   const main = document.querySelector("main");
+//   const body = document.querySelector("body");
+//   const media = window.matchMedia("(max-width: 766px)");
+
+// function setUpTopNav(e) {
+    
+//     if (e.matches) {
+//       console.log("is mobile");
+//       // topNavMenu.setAttribute("inert", "");
+//       topNavMenu.style.transition = "none";
+//     } else {
+//       console.log("is desktop");
+//       // topNavMenu.removeAttribute("inert");
+//       topNavMenu.style.transition = "";
+//       closeMobileMenu();
+//     }
+//   }
+  
+//   function openMobileMenu() {
+
+//     console.log("opening mobile menu");
+
+//     btnOpen.setAttribute("aria-expanded", "true");
+//     topNavMenu.classList.toggle("open", true);
+//     topNavMenu.removeAttribute("inert");
+//     topNavMenu.removeAttribute("style");
+//     main.setAttribute("inert", "");
+//     bodyScrollLockUpgrade.disableBodyScroll(body);
+//     btnClose.focus();
+
+//     console.log("mobile menu opened. Inert removed", !topNavMenu.hasAttribute("inert"));
+//   }
+  
+//   function closeMobileMenu() {
+
+//     console.log("close mobile menu");
+
+//     btnClose.setAttribute("aria-expanded", "false");
+//     topNavMenu.classList.toggle("open", false);
+//     topNavMenu.setAttribute("inert", "");
+//     main.removeAttribute("inert");
+//     bodyScrollLockUpgrade.enableBodyScroll(body);
+//     btnOpen.focus();
+  
+//     setTimeout(() => {
+//       topNavMenu.style.transition = "none";
+//       console.log("mobile menu closed");
+//     }, 500);
+//   }
+  
+
+//     setUpTopNav(media);
+  
+//     btnOpen.addEventListener("click", openMobileMenu);
+//     btnClose.addEventListener("click", closeMobileMenu);
+  
+//     media.addEventListener("change", function (e) {
+//       console.log("media query change detected");
+//       setUpTopNav(e);
+//     });
+
+
   

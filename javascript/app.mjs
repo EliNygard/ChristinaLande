813e838466e37738074b0ae84671651aa474e22f
EliNygard <eli.nygard@gmail.com>
2024-07-17 17:41:59 +0200

    adding style, adding insta clicker

--- javascript/app.mjs
+++ javascript/app.mjs
@@ -8,4 +8,7 @@ initializeTopNav();
 const insta = document.getElementById("insta")
 insta.addEventListener("click", (event) => openInstagramApp(event, "sorryma"))
 
-
+const btnEmail = document.getElementById("email")
+btnEmail.addEventListener("click", () => {
+    window.location.href = "mailto: christinalande@gmail.com"
+})

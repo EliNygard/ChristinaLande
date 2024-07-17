c0f9f663e3c698c071a977a5d077c4185a27b702
EliNygard <eli.nygard@gmail.com>
2024-07-17 19:07:44 +0200

    working on thumbs hover effect

--- javascript/app.mjs
+++ javascript/app.mjs
@@ -8,6 +8,8 @@ initializeTopNav();
 const insta = document.getElementById("insta")
 insta.addEventListener("click", (event) => openInstagramApp(event, "sorryma"))
 
+
+// move to separate mjs file for about page?
 const btnEmail = document.getElementById("email")
 btnEmail.addEventListener("click", () => {
     window.location.href = "mailto: christinalande@gmail.com"

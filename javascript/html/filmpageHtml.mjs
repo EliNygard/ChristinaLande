5560e9f70d0dfb4a0ec161e2212351e16a923167
EliNygard <eli.nygard@gmail.com>
2024-07-22 15:16:47 +0200

    generating html

--- javascript/html/filmpageHtml.mjs
+++ javascript/html/filmpageHtml.mjs
@@ -15,11 +15,42 @@ export function generateFilmPageHtml(project) {
     imgPriContainer.classList.add("project-img-primary")
 
     const imgPrimary = document.createElement("img")
-    imgPrimary.src = project.images.image1.img
 
+    if (project.images && project.images.image1) {
+        imgPrimary.src = project.images.image1.img
+        imgPrimary.alt = project.images.image1.alt
+    }
+
+    const contentContainer = document.createElement("div")
+    contentContainer.classList.add("project-content-container")
+
+    const synopsis = document.createElement("p")
+    synopsis.classList.add("synopsis")
+    synopsis.textContent = project.synopsis
+
+    const credits = document.createElement("div")
+    credits.classList.add("credits")
+
+    const director = document.createElement("p")
+    director.classList.add("director")
+    director.textContent = `Written and directed by ${project.director}`
+
+    const producer = document.createElement("p")
+    producer.classList.add("producer")
+    producer.textContent = `Produced by ${project.producer}`
+
+    const photo = document.createElement("p")
+    photo.classList.add("photo")
+    photo.textContent = `Cinematography: ${project.cinematography}`
+
+    const border = document.createElement("div")
+    border.classList.add("border")
+
+    credits.append(director, producer, photo)
+    contentContainer.append(synopsis, credits)
     imgPriContainer.appendChild(imgPrimary)
     headings.append(h1, h2)
-    container.append(headings, imgPriContainer)
+    container.append(headings, imgPriContainer, contentContainer)
 
     return container
 }

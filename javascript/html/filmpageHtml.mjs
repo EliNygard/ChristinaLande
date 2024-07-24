f7a94efc6b7243fc8056e994f02b6e9e49950c2a
EliNygard <eli.nygard@gmail.com>
2024-07-24 11:09:17 +0200

    add btn links

--- javascript/html/filmpageHtml.mjs
+++ javascript/html/filmpageHtml.mjs
@@ -1,81 +1,107 @@
 export function generateFilmPageHtml(project) {
-    const section = document.getElementById("filmPage")
-    
-    const headings = document.createElement("div")
-    headings.classList.add("project-headings", "manrope-heading")
+  const section = document.getElementById("filmPage");
 
-    const h1 = document.createElement("h1")
-    h1.classList.add("uppercase")
-    h1.textContent = project.title
+  const headings = document.createElement("div");
+  headings.classList.add("project-headings", "manrope-heading");
 
-    const h2 = document.createElement("h2")
-    h2.textContent = project.genre
+  const h1 = document.createElement("h1");
+  h1.classList.add("uppercase");
+  h1.textContent = project.title;
 
-    const imgPriContainer = document.createElement("div")
-    imgPriContainer.classList.add("project-img-primary")
+  const h2 = document.createElement("h2");
+  h2.textContent = project.genre;
 
-    const imgPrimary = document.createElement("img")
+  const imgPriContainer = document.createElement("div");
+  imgPriContainer.classList.add("project-img-primary");
 
-    if (project.images && project.images.image1) {
-        imgPrimary.src = project.images.image1.img
-        imgPrimary.alt = project.images.image1.alt
-    }
+  const imgPrimary = document.createElement("img");
 
-    const contentContainer = document.createElement("div")
-    contentContainer.classList.add("project-content-container")
+  if (project.images && project.images.image1) {
+    imgPrimary.src = project.images.image1.img;
+    imgPrimary.alt = project.images.image1.alt;
+  }
 
-    const synopsis = document.createElement("p")
-    synopsis.classList.add("synopsis")
-    synopsis.textContent = project.synopsis
+  const contentContainer = document.createElement("div");
+  contentContainer.classList.add("project-content-container");
 
-    const credits = document.createElement("div")
-    credits.classList.add("credits")
+  const synopsis = document.createElement("p");
+  synopsis.classList.add("synopsis");
+  synopsis.textContent = project.synopsis;
 
-    const director = document.createElement("p")
-    director.classList.add("director")
-    director.textContent = `Written and directed by ${project.director}`
+  const credits = document.createElement("div");
+  credits.classList.add("credits");
 
-    const producer = document.createElement("p")
-    producer.classList.add("producer")
-    producer.textContent = `Produced by ${project.producer}`
+  const director = document.createElement("p");
+  director.classList.add("director");
+  director.textContent = `Written and directed by ${project.director}`;
 
-    const photo = document.createElement("p")
-    photo.classList.add("photo")
-    photo.textContent = `Cinematography: ${project.cinematography}`
+  const producer = document.createElement("p");
+  producer.classList.add("producer");
+  producer.textContent = `Produced by ${project.producer}`;
 
-    const border = document.createElement("div")
-    border.classList.add("border")
+  const photo = document.createElement("p");
+  photo.classList.add("photo");
+  photo.textContent = `Cinematography: ${project.cinematography}`;
 
-    const winnerContainer = document.createElement("div")
-    winnerContainer.classList.add("winner")
+  const border = document.createElement("div");
+  border.classList.add("border");
 
-    const winner1 = document.createElement("p")
-    winner1.textContent = project.winner
+  const winnerContainer = document.createElement("div");
+  winnerContainer.classList.add("winner");
 
-    const festivalsContainer = document.createElement("div")
-    festivalsContainer.classList.add("festivals")
+  const winner1 = document.createElement("p");
+  winner1.textContent = project.winner;
 
-    const h3 = document.createElement("h3")
-    h3.textContent = "Festivals"
+  const festivalsContainer = document.createElement("div");
+  festivalsContainer.classList.add("festivals");
 
-    const ulFestivals = document.createElement("ul")
+  const h3 = document.createElement("h3");
+  h3.textContent = "Festivals";
 
-    const festivals = project.festivals
-    festivals.forEach((festival) => {
-        const liFestival = document.createElement("li")
-        const pFestival = document.createElement("p")
-        pFestival.textContent = festival
-        liFestival.appendChild(pFestival)
-        ulFestivals.appendChild(liFestival)
-    })
+  const ulFestivals = document.createElement("ul");
 
-    festivalsContainer.append(h3, ulFestivals)
-    winnerContainer.appendChild(winner1)
-    credits.append(director, producer, photo)
-    contentContainer.append(synopsis, credits, border, winnerContainer, festivalsContainer)
-    imgPriContainer.appendChild(imgPrimary)
-    headings.append(h1, h2)
-    section.append(headings, imgPriContainer, contentContainer)
+  const festivals = project.festivals;
+  festivals.forEach((festival) => {
+    const liFestival = document.createElement("li");
+    const pFestival = document.createElement("p");
+    pFestival.textContent = festival;
+    liFestival.appendChild(pFestival);
+    ulFestivals.appendChild(liFestival);
+  });
 
-    
+  const linksContainer = document.createElement("div");
+  linksContainer.classList.add("links");
+
+  
+  const trailer = document.createElement("a");
+  trailer.setAttribute("id", "linkTrailer");
+  
+  const btnTrailer = document.createElement("button");
+  btnTrailer.textContent = "Watch Trailer";
+  
+  
+  const film = document.createElement("a");
+  film.setAttribute("id", "linkFilm");
+  
+  const btnFilm = document.createElement("button");
+  btnFilm.textContent = "Watch Film";
+  
+  
+  trailer.appendChild(btnTrailer);
+  film.appendChild(btnFilm);
+  linksContainer.append(trailer, film);
+  festivalsContainer.append(h3, ulFestivals);
+  winnerContainer.appendChild(winner1);
+  credits.append(director, producer, photo);
+  contentContainer.append(
+    synopsis,
+    credits,
+    border,
+    winnerContainer,
+    festivalsContainer,
+    linksContainer
+  );
+  imgPriContainer.appendChild(imgPrimary);
+  headings.append(h1, h2);
+  section.append(headings, imgPriContainer, contentContainer);
 }
